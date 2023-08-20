1e57019f850df7419d8e889316e398592ea1dde4
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-08-20 20:11:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -207,7 +207,7 @@ if __name__ == "__main__" :
    
     @app.on_message(filters.incoming & filters.command(["help", f"help@{BOT_USERNAME}"]))
     async def help_message(app, message):
-        await message.reply_text("Hi, I am <b>Video Encoder bot</b>\n\n➥ Send me your telegram files\n➥ I will encode them one by one as I have <b>queue feature</b>\n➥ Just send me the jpg/pic and it will be set as your custom thumbnail \n➥ For ffmpeg lovers - u can change crf by /eval crf.insert(0, 'crf value')\n➥ Contact ☆ @Sensei_Rimuru \n\n🏷<b>Maintained By : @Anime_Sensei_Network t</b>", quote=True)
+        await message.reply_text("Hi, I am <b>Video Encoder bot</b>\n\n➥ Send me your telegram files\n➥ I will encode them one by one as I have <b>queue feature</b>\n➥ Just send me the jpg/pic and it will be set as your custom thumbnail \n➥ For ffmpeg lovers - u can change crf by /eval crf.insert(0, 'crf value')\n➥ Contact ☆ @ninja_naruto_sai_2 \n\n🏷<b>Maintained By : @ninja_naruto_sai_2 t</b>", quote=True)
   
     @app.on_message(filters.incoming & filters.command(["log", f"log@{BOT_USERNAME}"]))
     async def help_message(app, message):
