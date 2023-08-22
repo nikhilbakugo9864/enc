5465130e7c89b5ee774b4c98fd53bb04a84b727d
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-08-22 16:48:34 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -23,7 +23,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ []"
+    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [@anime_in_30mb]"
 
     COMPRESS_PROGRESS = "🕛 ETA: {}\n♻️ Progress: {}%"
 
